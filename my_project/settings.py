0f888ac765e351d2461e3a43c6f35d23fa46c38e
Tripta Grey <tripta.grey@gmail.com>
2024-12-17 14:52:52 +0000

    update code for deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a34yny9e4_lf1lu@*@d28*v5)=9tw6mc_g&^0^iga)=yyw&e7-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-tgrey2024-djangoproject-9izfdr20fts.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-tgrey2024-djangoproject-9izfdr20fts.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition

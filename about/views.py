b66da7eb93703b79c5bef521924106ceb6a65c4c
Tripta Grey <tripta.grey@gmail.com>
2024-12-17 10:58:12 +0000

    add views in about app and wire up urls

--- about/views.py
+++ about/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def about_me(request):
+    return HttpResponse("This is a page about me. I'm learning Django and it's tricky.")
